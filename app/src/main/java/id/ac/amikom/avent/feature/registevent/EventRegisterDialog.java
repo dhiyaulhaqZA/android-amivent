8bf1920d7dc0369729cb58878e8503c4fa47b05b
dhiyaulhaqZA <dhiya.alamsyah@students.amikom.ac.id>
2017-12-27 00:13:12 +0700

    [ULHAQ] Add event register dialog

---
+++ app/src/main/java/id/ac/amikom/avent/feature/registevent/EventRegisterDialog.java
@@ -0,0 +1,37 @@
+package id.ac.amikom.avent.feature.registevent;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import id.ac.amikom.avent.R;
+
+/**
+ * Created by dhiyaulhaqza on 12/27/17.
+ */
+
+public class EventRegisterDialog extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_event_register, null, false);
+        builder.setView(view)
+                .setPositiveButton("Register", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        return builder.create();
+    }
+}
