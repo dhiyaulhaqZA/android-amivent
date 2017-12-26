9393378d64a35c68b1dcecb59f12473243a2161f
MuhammadArif686 <arief86.ma@gmail.com>
2017-12-25 21:05:37 -0800

    [ARIF] delete menu item main activity

--- app/src/main/java/id/ac/amikom/avent/main/MainActivity.java
+++ app/src/main/java/id/ac/amikom/avent/main/MainActivity.java
@@ -79,35 +79,6 @@ public class MainActivity extends BaseActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
-        switch (id) {
-            case R.id.action_profile:
-                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
-                return true;
-            case R.id.action_sign_out:
-                FirebaseAuth.getInstance().signOut();
-                UserPref userPref = new UserPref(MainActivity.this);
-                userPref.cleanUpUserPref();
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
